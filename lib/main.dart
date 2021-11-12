7db2dcb34ea6c6a0b31c11bda6a6ccdaa598d340
Yue-plus <Yue_plus@foxmail.com>
2021-11-12 15:20:45 +0800

    启动逻辑

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'welcome/welcome.dart';
+import 'package:super_halo/tourist/tourist.dart';
 
-void main() => runApp(
-  const MaterialApp(
-    title: "SuperHalo",
-    home: Welcome()
-  )
-);
+void main() async {
+  SharedPreferences sp = await SharedPreferences.getInstance();
+  final hostLink = sp.getString('HOST_LINK');
+  final accessKey = sp.getString('ACCESS_KEY');
+
+  if (hostLink != null && accessKey != null) {
+    return runApp(const MaterialApp(title: 'SuperHalo', home: Tourist()));
+  } else {
+    return runApp(const MaterialApp(title: '连接到 Halo 主机', home: Welcome()));
+  }
+}
