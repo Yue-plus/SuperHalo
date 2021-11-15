0c06e25780c9500d6af0afa61dd7751e6200d9bf
Yue-plus <Yue_plus@foxmail.com>
2021-11-15 15:51:26 +0800

    主页改为有状态组件

--- lib/tourist/tourist.dart
+++ lib/tourist/tourist.dart
@@ -1,20 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class Tourist extends StatelessWidget {
+class Tourist extends StatefulWidget {
   const Tourist({Key? key}) : super(key: key);
 
   @override
+  State<StatefulWidget> createState() => _TouristState();
+}
+
+class _TouristState extends State<Tourist> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('SuperHalo'),
-        leading: const Icon(
-          Icons.menu,
-          color: Colors.white,
-          size: 24.0,
-          semanticLabel: '菜单',
-        )
-      )
+      appBar: AppBar(title: const Text('SuperHalo')),
+      drawer: const Drawer(
+        child: Text('data'),
+      ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    () async {
+      final sp = await SharedPreferences.getInstance();
+      final hostLink = sp.getString('HOST_LINK');
+      final accessKey = sp.getString('ACCESS_KEY');
+
+      print('asdasdasda');
+
+      if (hostLink == null && accessKey == null) {
+        Navigator.pushNamed(context, '/login');
+      }
+    };
+    // TODO: 检查登入状态
   }
 }
