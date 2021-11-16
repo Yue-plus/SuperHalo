a3f5c17777ffef3079a8d70f983ac699d717cdf1
Yue-plus <Yue_plus@foxmail.com>
2021-11-16 19:29:01 +0800

    update tag_controller.dart

--- lib/halo_api/content/tag_controller.dart
+++ lib/halo_api/content/tag_controller.dart
@@ -19,8 +19,84 @@ class Tag {
       fullPath = json['fullPath'];
 }
 
+class ListsPostsByTagSlugContent {
+  int id;
+  String title;
+  String status;
+  String slug;
+  String editorType;
+  int updateTime;
+  int createTime;
+  int editTime;
+  String? metaKeywords;
+  String? metaDescription;
+  String fullPath;
+  String summary;
+  String thumbnail;
+  int visits;
+  bool disallowComment;
+  String password;
+  String template;
+  String topPriority;
+  int likes;
+  int wordCount;
+  int commentCount;
+  List<Tag> tags;
+  List categories;
+  String metas;
+  bool topped;
+
+  ListsPostsByTagSlugContent.formJson(Map<String, dynamic> json)
+    : id = json['id'],
+      title = json['title'],
+      status = json['status'],
+      slug = json['slug'],
+      editorType = json['editorType'],
+      updateTime = json['updateTime'],
+      createTime = json['createTime'],
+      editTime = json['editTime'],
+      metaKeywords = json['metaKeywords'],
+      metaDescription = json['metaDescription'],
+      fullPath = json['fullPath'],
+      summary = json['summary'],
+      thumbnail = json['thumbnail'],
+      visits = json['visits'],
+      disallowComment = json['disallowComment'],
+      password = json['password'],
+      template = json['template'],
+      topPriority = json['topPriority'],
+      likes = json['likes'],
+      wordCount = json['wordCount'],
+      commentCount = json['commentCount'],
+      tags = json['tags'],
+      categories = json['categories'],
+      metas = json['metas'],
+      topped = json['topped'];
+}
+
 class ListsPostsByTagSlug {
-  // TODO: 待完成类
+  int pages;
+  int total;
+  int page;
+  int rpp;
+  bool hasNext;
+  bool hasPrevious;
+  bool isFirst;
+  bool isLast;
+  bool isEmpty;
+  bool hasContent;
+
+  ListsPostsByTagSlug.formJson(Map<String, dynamic> json)
+    : pages = json['pages'],
+      total = json['total'],
+      page = json['page'],
+      rpp = json['rpp'],
+      hasNext = json['hasNext'],
+      hasPrevious = json['hasPrevious'],
+      isFirst = json['isFirst'],
+      isLast = json['isLast'],
+      isEmpty = json['isEmpty'],
+      hasContent = json['hasContent'];
 }
 
 class TagController {
