1ea6338f99476773c48132bdeee164fa06139821
Yue-plus <Yue_plus@foxmail.com>
2021-11-16 10:16:42 +0800

    异步请求数据

--- lib/tourist/tourist.dart
+++ lib/tourist/tourist.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:super_halo/halo_api/content/user_controller.dart';
+
+/// 检查登入状态
+Future _checkBaseInfo(BuildContext context) async {
+  final sp = await SharedPreferences.getInstance();
+  final hostLink = sp.getString('HOST_LINK');
+  final accessKey = sp.getString('ACCESS_KEY');
+
+  if (hostLink == null && accessKey == null) {
+    Navigator.pushNamed(context, '/login');
+  }
+}
 
 class Tourist extends StatefulWidget {
   const Tourist({Key? key}) : super(key: key);
@@ -9,6 +21,16 @@ class Tourist extends StatefulWidget {
 }
 
 class _TouristState extends State<Tourist> {
+  late Future<BloggerProfile?> bloggerProfile;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkBaseInfo(context);
+
+    bloggerProfile = UserController.getsBloggerProfile();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,24 +38,19 @@ class _TouristState extends State<Tourist> {
       drawer: const Drawer(
         child: Text('data'),
       ),
+      body: Center(
+        child: FutureBuilder<BloggerProfile?>(
+          future: bloggerProfile,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(snapshot.data!.username);
+            } else if (snapshot.hasError) {
+              return Text(snapshot.error.toString());
+            }
+            return const Text('loading...');
+          },
+        ),
+      )
     );
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    () async {
-      final sp = await SharedPreferences.getInstance();
-      final hostLink = sp.getString('HOST_LINK');
-      final accessKey = sp.getString('ACCESS_KEY');
-
-      print('asdasdasda');
-
-      if (hostLink == null && accessKey == null) {
-        Navigator.pushNamed(context, '/login');
-      }
-    };
-    // TODO: 检查登入状态
   }
 }
