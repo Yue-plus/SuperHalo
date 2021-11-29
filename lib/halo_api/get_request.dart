ce0e49322ab404931ee3867daadca064a8ef537b
Yue-plus <Yue_plus@foxmail.com>
2021-11-29 12:18:00 +0800

    格式调整

--- lib/halo_api/get_request.dart
+++ lib/halo_api/get_request.dart
@@ -34,16 +34,22 @@ class GetRequest {
       final responseBody = jsonDecode(response.body);
 
       if (response.statusCode == 200) {
-        return GetRequest(response.statusCode, responseBody['message'],
-            responseBody['devMessage'], jsonEncode(responseBody['data']));
+        return GetRequest(
+          response.statusCode,
+          responseBody['message'],
+          responseBody['devMessage'],
+          jsonEncode(responseBody['data']),
+        );
       } else {
         if (context != null) {
           // TODO: 加个加载动画
           return showDialog(
-              context: context,
-              builder: (BuildContext context) => AlertDialog(
-                  title: Text(response.statusCode.toString()),
-                  content: Text(responseBody['message'])));
+            context: context,
+            builder: (BuildContext context) => AlertDialog(
+              title: Text(response.statusCode.toString()),
+              content: Text(responseBody['message']),
+            ),
+          );
         } else {
           return null;
         }
